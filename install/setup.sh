1833f62c926d6c572e68a2d33a0a4457ad771f42
Thiago Francisco <nullck@gmail.com>
2020-06-26 16:21:08 -0300

    getting this repo more organized

--- install/setup.sh
+++ install/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CREATE_REGO="kubectl create configmap" 
+CREATE_REGO="kubectl create configmap"
 
 kubectl create ns opa
 kubectl create -n opa secret tls opa-server --cert=server.crt --key=server.key
@@ -10,7 +10,7 @@ kubectl label ns kube-system openpolicyagent.org/webhook=ignore
 kubectl label ns opa openpolicyagent.org/webhook=ignore
 
 ## rego rules
-$CREATE_REGO labels -n opa --from-file=examples/labels.rego
-$CREATE_REGO protect-namespace -n opa --from-file=examples/protect-namespace.rego
-$CREATE_REGO register -n opa --from-file=examples/register-deployment-regex-test.rego
+$CREATE_REGO labels -n opa --from-file=../examples/labels.rego
+$CREATE_REGO protect-namespace -n opa --from-file=../examples/protect-namespace-alternative-version.rego
+$CREATE_REGO register -n opa --from-file=../examples/register-deployment-regex-test.rego
 
